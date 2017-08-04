1bd0d80e051f769ec36c00e80b0253ab3973c51e
Dave Albert <dave@one15digital.com>
2017-08-04 17:53:47 +0100

    fix typo, and update index script

--- textfiles/make_index.sh
+++ textfiles/make_index.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "">index.html;find . -name "*.txt" -type f -exec echo '<a href="{}">{}</a>' >>index.html \;
+echo "<html><body>">index.html;find . -name "*.txt" -type f -exec echo '<a href="{}">{}</a><br />' >>index.html \; ;echo "</body></html>" >> index.html
 
