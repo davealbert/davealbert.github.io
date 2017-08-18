cb72b973ec8c978997f2e59af1d9860a8aea4f51
Dave Albert <dave@one15digital.com>
2017-08-18 17:50:05 +0100

    main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+$(() => {
+    $.ajax('https://9u0gb30id0.execute-api.eu-west-1.amazonaws.com/Prod/counter?site=davealberttext')
+        .done((res) => {
+            console.log(res);
+            $('#counter').html(res);
+        });
+});
+
