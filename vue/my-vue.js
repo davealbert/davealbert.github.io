62484b672be7230ff0bbd6a9d39c0d034cd883a6
Dave Albert <dave@one15digital.com>
2018-10-26 13:06:41 +0100

    vue

--- vue/my-vue.js
+++ vue/my-vue.js
@@ -1,6 +1,23 @@
+
+Vue.component('todo-item', {
+    props: ['todo', 'thing'],
+    template: '<li>{{ todo.name }} {{ thing }}</li>'
+});
+
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello World!'
+        message: 'Hello World!',
+        otherThing: 'frack-tastic',
+        arr: [
+            {name: 'foo', id: 1},
+            {name: 'bar', id: 2},
+            {name: 'baz', id: 3},
+        ]
+    },
+    methods: {
+        doIt: () => {
+            alert(1);
+        }
     }
 });
