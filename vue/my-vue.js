fa0ca919e4e85a4fef67fdd57c062e86fd86328e
Dave Albert <dave@one15digital.com>
2018-12-29 13:04:11 +0000

    vue updates

--- vue/my-vue.js
+++ vue/my-vue.js
@@ -1,23 +1,54 @@
 
 Vue.component('todo-item', {
-    props: ['todo', 'thing'],
-    template: '<li>{{ todo.name }} {{ thing }}</li>'
+    props: ['name', 'id'],
+    template: '<li>{{ name }} {{ id }}</li>'
+});
+
+const Child = Vue.component('child', {
+    data: function() {
+        return {
+            count: 0
+        }
+    },
+    template: '<div>You clicked me {{ count }} times.</div>',
+
 });
 
 var app = new Vue({
     el: '#app',
+    components: {
+        Child
+    },
     data: {
+        count: 0,
         message: 'Hello World!',
         otherThing: 'frack-tastic',
-        arr: [
+        classes: [
+            {name: 'FooClass', id: 4},
+            {name: 'BarClass', id: 5},
+            {name: 'BazClass', id: 6},
+        ],
+        items: [
             {name: 'foo', id: 1},
             {name: 'bar', id: 2},
             {name: 'baz', id: 3},
         ]
     },
     methods: {
-        doIt: () => {
-            alert(1);
+        say (what) {
+            console.log(what + ' ' + this.count);
+        },
+        updateComponentData(reference, both) {
+            this.count++;
+            console.log(reference);
+
+            if (!both) {
+                this.$refs[reference].count = this.count;
+            } else {
+                for (ref in this.$refs) {
+                    this.$refs[ref].count = this.count;
+                }
+            }
         }
     }
 });
